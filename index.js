bad9cb7ba471a37bd6e823cd772c2032e7a8277c
Andres Felipe Bejarano <af.bejarano@uniandes.edu.co>
2020-02-09 23:36:35 -0500

    asd

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var path = require('path');
+var cookieParser = require('cookie-parser');
+var hogan = require('hogan.js');
+const fs = require('fs');
 const { Client } = require('pg');
 require('dotenv').config();
 
@@ -11,32 +14,48 @@ const client = new Client({
 client.connect().catch((err) => console.error(err))
 
 var app = express();
-
+app.set('views', path.join('./html'));
+app.set('layout', 'layout');
+app.set('view engine', 'html');
+app.enable('view cache');
+app.engine('html', hogan);
 app.use(express.urlencoded())
+app.use(cookieParser());
 
 app.post('/login', function (req, res) {
-  const text = 'SELECT * FROM USERS WHERE US = $1 AND PASS = $2'
+  const text = 'SELECT * FROM USERS WHERE USER_A = $1 AND PASS = $2'
   const values = [req.body.user, req.body.password]
   client.query(text, values, (err, resp) => {
     if (err) {
       console.log(err.stack)
     } else {
       if (resp.rows[0]){
-        res.sendFile(path.join(__dirname + '/entro.html'));
+        res.cookie('user',values[0])
+        home(res)
       }
       else{
-        res.sendFile(path.join(__dirname + '/noesta.html'));
+        res.sendFile(path.join(__dirname + '/html/noesta.html'));
       }
     }
   })
 });
 
+function home(res){
+  var f;
+  data = fs.readFileSync('./html/tasks.html');
+  template = hogan.compile(data);
+  res.send(template.render({'tasks':[{'id':'hola'},{'id':'hola1'},{'id':'hola2'}]}));
+}
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  if(req.cookies.user)
+    home(res);
+  else
+    res.sendFile(path.join(__dirname + '/html/index.html'));
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(6969, function () {
+  console.log('Example app listening on port 6969!');
 });
 
 
